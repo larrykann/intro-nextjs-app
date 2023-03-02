590d2d41b116ebba12840cb1512f6b8cec3846f6
wlkIV <kanndide@gmail.com>
2023-03-02 14:52:32 -0500

    added pages/routes and styling to homepage using css modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-
+import styles from '../styles/home.module.css'
 
 export default async function Home() {
-  return <div>hello</div>
+  return <div classname={styles.home}>
+    <div>
+      <div>name</div>
+      <div>blog</div>
+      <div>contact</div>
+    </div>
+  </div>
 }
