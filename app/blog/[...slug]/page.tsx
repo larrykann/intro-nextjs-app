c62734712c5d610ffab4ff180a48cb6ef5dc175c
wlkIV <kanndide@gmail.com>
2023-03-02 17:09:01 -0500

    created contact page and form. created route and used 'use client' for client component.

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,5 +1,5 @@
 export default async function BlogPost() {
     return <div>
-        <div>Contact Page</div>
+        <div>Post</div>
     </div>
   }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,28 @@
-export default async function Contact() {
+// 'use client' would typically be put in the farthest possible component that needs it. In ths scenarion, a button could be made and 'use client' called there.
+'use client'
+
+import { useState } from "react"
+
+export default function Contact() {
+    const [email, setEmail] = useState('')
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        await fetch('/api/contact', {
+            method: 'POST',
+            body: JSON.stringify({email}),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+        console.log('hey')
+    }
+
     return <div>
-        <div>Contact Page</div>
+        <form onSubmit={handleSubmit}>
+            <input type="email" required value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <button type="submit">Submit</button>
+        </form>
     </div>
   }
