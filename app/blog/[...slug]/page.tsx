0d2a38b52706e32156367d6c0cd3036078d06860
wlkIV <kanndide@gmail.com>
2023-03-02 15:45:41 -0500

    adding global styles, links to home, blog page functions, etc

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,5 +1,5 @@
-export default async function blogpost() {
+export default async function BlogPost() {
     return <div>
-        <div>Blog Post</div>
+        <div>Contact Page</div>
     </div>
   }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import styles from '../styles/home.module.css'
+import Link from 'next/link'
 
 export default async function Home() {
   return <div className={styles.home}>
     <div>
-      <div>name</div>
-      <div>blog</div>
-      <div>contact</div>
+      <div>
+        <h1>My name is Larry Kann</h1>
+      </div>
+      <div>
+        <Link href="/blog">Checkout my Blog</Link>
+      </div>
+      <div>
+      <Link href="/contact">Contact Me</Link>
+      </div>
     </div>
   </div>
 }
